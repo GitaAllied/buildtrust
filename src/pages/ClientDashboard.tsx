0db61c9fac37e935913aff657a0682cf7af630a9
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-02-03 14:22:00 +0100

    Changed placeholder image

--- src/pages/ClientDashboard.tsx
+++ src/pages/ClientDashboard.tsx
@@ -31,7 +31,7 @@ import {
 } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-const PROJECT_PLACEHOLDER = 'https://placehold.net/600x400.png';
+const PROJECT_PLACEHOLDER = 'https://placehold.net/main.svg';
 
 const ClientDashboard = () => {
   const [activeTab, setActiveTab] = useState("dashboard");
